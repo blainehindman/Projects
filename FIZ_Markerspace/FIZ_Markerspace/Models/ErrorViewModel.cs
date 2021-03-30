23b0e937a842d5cdfab4eb8b46032dcdfea3553f
Blaine <blainemichaelhindman@gmail.com>
2021-03-30 13:27:32 -0700

    Added FIZ Project

---
+++ FIZ_Markerspace/FIZ_Markerspace/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FIZ_Markerspace.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
